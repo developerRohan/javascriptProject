020d13af5cf93ca7551941e057b94775a020ed62
MY RAILS <jhambrohan44@gmail.com>
2017-06-14 00:38:08 +0530

    get the input values and called the function

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
-var controller = (function(budgetCtrl, UICtrl) {
-
-    var setupEventListeners = function() {
-        var DOM = UICtrl.getDOMstrings();
-
-        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-
-         //because we want the data to add on enter key
-         document.addEventListener('keypress', function(event) {
-             if (event.keyCode === 13 || event.which === 13) {
-                 ctrlAddItem();
-             }
-         });
-      }
-})(budgetController, UIController);
-
-
 var budgetController = (function(){
 
-});
+})();
 
 
 var UIController = (function(){
+ var DOMstrings = {
+   inputType: '.add__type',
+   inputDescription: '.add__description',
+   inputValue: '.add__value',
+   inputBtn: '.add__btn',
+ };
 
-});
+
+  return{
+    getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+  };
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+  var ctrlAddItem = function(){
+    //get the input value
+    var input = UICtrl.getInput();
+    console.log(input);
+  }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    //because we want the data to add on enter key
+     document.addEventListener('keypress', function(event) {
+         if (event.keyCode === 13 || event.which === 13) {
+             ctrlAddItem();
+         }
+       });
+
+})(budgetController, UIController);
