4da92916907eb4ecb903a4d89b463b6392465ef9
MY RAILS <jhambrohan44@gmail.com>
2017-06-14 15:12:15 +0530

    added current month in the app

--- app.js
+++ app.js
@@ -128,7 +128,8 @@ var UIController = (function(){
    incomeLabel: '.budget__income--value',
    expensesLabel: '.budget__expenses--value',
    percentageLabel: '.budget__expenses--percentage',
-   container: '.container'
+   container: '.container',
+   dateLabel: '.budget__title--month'
  };
 
 
@@ -195,6 +196,19 @@ var UIController = (function(){
           }
 
       },
+
+      displayMonth: function() {
+          var now, months, month, year;
+
+          now = new Date();
+          //var christmas = new Date(2016, 11, 25);
+
+          months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+          month = now.getMonth();
+
+          year = now.getFullYear();
+          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -276,6 +290,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return{
     init: function(){
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
           budget: 0,
           totalInc: 0,
